17531829b048450f961663fd88cce8801dc4ac31
CYX15916011519 <50003846+CYX15916011519@users.noreply.github.com>
2019-06-14 10:03:14 +0800

    包装余数

---
+++ src/JIT.DIME2Barcode/Entities/VW_YSQty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace JIT.DIME2Barcode.Entities
+{
+    public class VW_YSQty
+    {
+        public string FBillNo { get; set; }
+        public string FBiller { get; set; }
+        public string FName { get; set; }
+        public DateTime FDate { get; set; }
+        public string FBillNo2 { get; set; }
+        public string BatchNum { get; set; }
+        public int FYSQty { get; set; }
+        public string FInspector { get; set; }
+        public DateTime FInspectTime { get; set; } 
+    }
+}
